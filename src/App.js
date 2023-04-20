c0acf0949d6122e275132fab6c63ac62591b1af2
Charlie Hodgkinson <charlotte.hodgkinson4@gmail.com>
2023-04-20 15:35:20 +0100

    WIP: fix 2 canvases

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { setup, preload } from "./sketches/lineSketch";
 import style from "./style.css";
 
 const App = () => {
+  // const elementRef = useRef(null);
   return (
     <div className="app" css={{ width: "100%", height: "100%" }}>
       <Sketch setup={setup} preload={preload} />
